f05e68f9d40fac9b29797b00d5d17eeda8580af0
danang wijaya <danangwijaya750@gmail.com>
2021-04-29 16:00:14 +0700

    Initial Commit

---
+++ Githup App/Githup_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Githup_AppApp.swift
+//  Githup App
+//
+//  Created by Danang Wijaya on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Githup_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
